d38b9e526710fa28fcee506da021866915d01545
Pricing-Logic <pricinglogic@fastmail.com>
2026-03-17 15:31:42 +0000

    feat: add AppCommand types for command palette registry

---
+++ Sources/TermGrid/CommandPalette/AppCommand.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+enum CommandScope {
+    case global
+    case cell
+}
+
+struct CommandContext {
+    let focusedCellID: UUID?
+    let cellUIState: CellUIState?
+    let store: WorkspaceStore
+    let sessionManager: TerminalSessionManager
+}
+
+struct AppCommand: Identifiable {
+    let id: String
+    let title: String
+    let icon: String
+    let scope: CommandScope
+    var isAvailable: (CommandContext) -> Bool = { _ in true }
+    let action: (CommandContext) -> Void
+}
